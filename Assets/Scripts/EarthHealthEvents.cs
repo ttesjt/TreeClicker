5781f5e6cfd07714c311ba27952ef5d0d52d5a30
Weiyan Tao <taoweiyan1998@gmail.com>
2023-11-11 22:33:24 -0800

    animation

---
+++ Assets/Scripts/EarthHealthEvents.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EarthHealthEvents : MonoBehaviour
+{
+    public List<float> eventsHealthRatio;
+    public List<UnityEvent> events;
+
+    public Animator earthContainerAnimator;
+
+    public void ShowEarthContainer() {
+        earthContainerAnimator.SetBool("Appeared", true);
+    }
+
+    public void CheckForEvents(float healthRatio) { // 0 - 1
+        Debug.Log(healthRatio);
+        healthRatio = Mathf.Clamp(healthRatio, 0, 1f);
+        if (eventsHealthRatio.Count > 0 && events.Count > 0) {
+            if (eventsHealthRatio[0] >= healthRatio) {
+                events[0].Invoke();
+                eventsHealthRatio.RemoveAt(0);;
+                events.RemoveAt(0);;
+            }
+        }
+    }
+}
